7124638961a639d2a73aa46029aef58ba32b7492
MrAnkan <information@d-ank.com>
2015-12-22 10:44:05 +0100

    Oto dir och oto lang

--- index.php
+++ index.php
@@ -24,68 +24,98 @@
 header("Access-Control-Allow-Origin: *");
 
 // If api key is correct connect to database and make sure it is coded in utf8
-if ($_GET['apikey'] == " ") { //SET
-   
+if ($_GET['apikey'] == " ") { //Change
+
     header("Content-type: application/json; charset=utf-8");
-    $servername = " "; //SET
-    $username = " "; //SET
-    $password = " "; //SET
-    $database = " "; //SET
+    $servername = " "; //Change
+    $username = " "; //Change
+    $password = " "; //Change
+    $database = " "; //Change
 
     mysqli_set_charset("utf8");
     $con=mysqli_connect("$servername",$username, $password, $database); 
-      // Check connection
-      if (mysqli_connect_errno())
-      {
-       echo "Failed to connect to MySQL: " . mysqli_connect_error();
-      }
-     if ($_GET['oto_directory'] == "1") {
-              $query = "SELECT * FROM `eter0_oto_directory` WHERE activated=1 ORDER BY `id` ASC";
-              mysqli_query("SET names utf8;");
-              mysqli_set_charset("utf8");
-              mysqli_set_charset('utf8');
-              $rs = mysqli_query($con,$query);
+    // Check connection
+    if (mysqli_connect_errno())
+    {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+    if ($_GET['oto_directory'] == "1") {
+        $query = "SELECT * FROM `eter0_oto_directory` WHERE activated=1 ORDER BY `id` ASC";
+        mysqli_query("SET names utf8;");
+        mysqli_set_charset("utf8");
+        mysqli_set_charset('utf8');
+        $rs = mysqli_query($con,$query);
 
-            if (mysqli_num_rows($rs) > 0) {
-                $response["oto_directory"]= array();
+        if (mysqli_num_rows($rs) > 0) {
+            $response["oto_directory"]= array();
 
-                //Construct the Startpage array    
-                while($obj = mysqli_fetch_array($rs)) {
-                  $info = array(
-                        "id" => $obj['id'], "title" => utf8_encode($obj['title']), "lang" => utf8_encode($obj['lang']), "school_name" => utf8_encode($obj['school_name']), "school_id" => utf8_encode($obj['school_id']), "school_domain" => utf8_encode($obj['school_domain'])
+            //Construct the oto_directory array    
+            while($obj = mysqli_fetch_array($rs)) {
+
+                $info = array(
+                    "id" => $obj['id'], "title" => utf8_encode($obj['title']), "school_logo" => utf8_encode($obj['school_logo']), "lang" => utf8_encode($obj['lang']), "school_name" => utf8_encode($obj['school_name']), "school_id" => utf8_encode($obj['school_id']), "school_domain" => utf8_encode($obj['school_domain'])
                 );
-                 array_push($response["oto_directory"], $info);
-                }
-
-                $response["success"] = 1;
-                header("Content-type: application/json; charset=utf-8");
-                header("Access-Control-Allow-Origin: *");
-                echo json_encode($response);
-            } else {
-                $response["success"] = 0;
-                $response["message"] = "No sites found";
-                header("Content-type: application/json; charset=utf-8");
-                header("Access-Control-Allow-Origin: *");
-                echo json_encode($response);
+                array_push($response["oto_directory"], $info);
             }
-        }   
-      else if ($_GET['courses-slider'] == "1") {
-          $query = "SELECT * FROM `eter0_eter_courses_slider` ORDER BY `id` ASC";
-          mysqli_query("SET names utf8;");
-          mysqli_set_charset("utf8");
-          mysqli_set_charset('utf8');
-          $rs = mysqli_query($con,$query);
+
+            $qLang = "SELECT * FROM `eter0_oto_language` ORDER BY `id` ASC";
+            $rsLang = mysqli_query($con,$qLang);
+
+            if (mysqli_num_rows($rsLang) > 0) {
+                $langArray["oto_language"]= array();
+
+                //Construct the oto_lang array    
+                while($obj = mysqli_fetch_array($rsLang)) {
+                    $Lang = array(
+                        "id" => $obj['id'], "language" => utf8_encode($obj['language']), "original" => utf8_encode($obj['original']), "translation" => utf8_encode($obj['translation']), "location" => utf8_encode($obj['location'])
+                    );
+                    array_push($langArray["oto_language"], $Lang);
+                }
+            }
+
+            //Loop through all schools     
+            foreach($response['oto_directory'] as $index => $dir) {
+                //Loop through all posts in the wordpress api for courses category
+                foreach($langArray["oto_language"] as $p => $arr) {
+                    //Loop through the posts categories
+
+                    // If the category id matches the course id push the posts in to elments array ´
+                    if($dir['lang'] == $arr['language']) {               
+                        array_push($response['oto_directory'][$index]['oto_language'][] = $arr);
+                    }
+
+                }
+            }
+
+            $response["success"] = 1;
+            header("Content-type: application/json; charset=utf-8");
+            header("Access-Control-Allow-Origin: *");
+            echo json_encode($response);
+        } else {
+            $response["success"] = 0;
+            $response["message"] = "No sites found";
+            header("Content-type: application/json; charset=utf-8");
+            header("Access-Control-Allow-Origin: *");
+            echo json_encode($response);
+        }
+    } 
+    else if ($_GET['courses-slider'] == "1") {
+        $query = "SELECT * FROM `eter0_eter_courses_slider` ORDER BY `id` ASC";
+        mysqli_query("SET names utf8;");
+        mysqli_set_charset("utf8");
+        mysqli_set_charset('utf8');
+        $rs = mysqli_query($con,$query);
 
         if (mysqli_num_rows($rs) > 0) {
             $response["courses_slider"]= array();
 
             //Construct the Startpage array    
             while($obj = mysqli_fetch_array($rs)) {
-              $info = array(
+                $info = array(
                     "id" => $obj['id'], "title" => utf8_encode($obj['title']), "course" => utf8_encode($obj['course']), "image_url" => utf8_encode($obj['image_url']), "on_link" => utf8_encode($obj['on_link']), "row" => $obj['row'], "position" => $obj['position'], "description" => utf8_encode($obj['content'])
 
-            );
-             array_push($response["courses_slider"], $info);
+                );
+                array_push($response["courses_slider"], $info);
             }
 
             $response["success"] = 1;
@@ -100,7 +130,7 @@ if ($_GET['apikey'] == " ") { //SET
             echo json_encode($response);
         }
     }
-else if($_GET['list-taxonomy']== '1') {
+    else if($_GET['list-taxonomy']== '1') {
         if ($_GET['type'] == 'category') {
             $query = "SELECT * FROM `eter0_term_taxonomy` WHERE taxonomy='category' AND parent != 43 AND term_id !=43 AND term_id !=30 AND term_id !=1 ORDER BY `term_id` ASC";
         } else if ($_GET['type'] == 'tag') {
@@ -111,10 +141,10 @@ else if($_GET['list-taxonomy']== '1') {
         mysqli_set_charset("utf8");
         mysqli_set_charset('utf8');
         $rs = mysqli_query($con,$query);
-        
+
         if (mysqli_num_rows($rs) > 0) {
             $response["list_taxonomy"]= array();
-            
+
             while($obj = mysqli_fetch_array($rs)) {
                 $s_query = "SELECT * FROM `eter0_terms` WHERE term_id=".$obj['term_id']." ORDER BY `term_id` ASC";
                 mysqli_query("SET names utf8;");
@@ -123,8 +153,8 @@ else if($_GET['list-taxonomy']== '1') {
                 $rs2 = mysqli_query($con,$s_query);
                 while($objs = mysqli_fetch_array($rs2)) {
                     $info = array("id" => $obj['term_id'], "description" => utf8_encode($obj['description']), "name" => utf8_encode($objs['name']), "parent" => utf8_encode($obj['parent']), "post_count" => utf8_encode($obj['count']), "slug" => utf8_encode($objs['slug'])
-                         );
-                array_push($response["list_taxonomy"], $info);  
+                                 );
+                    array_push($response["list_taxonomy"], $info);  
                 }    
             }
             $response["success"] = 1;
@@ -139,22 +169,22 @@ else if($_GET['list-taxonomy']== '1') {
             echo json_encode($response);
         }   
     } 
-else if($_GET['post_vote'] == "1") {
+    else if($_GET['post_vote'] == "1") {
         if ($_GET['post_id'] > 0) {
-            
-            if ($_GET['new_vote'] == "1") {
-            //Insert to vote table, prepare the query
-            $query = "INSERT INTO `rudbeck_info`.`eter0_wti_like_post` (`id`, `post_id`, `value`, `date_time`, `ip`, `user_id`) VALUES (NULL, '".$_GET['post_id']."', '1', '', '0', '0');";
 
-            $rs = mysqli_query($con,$query);
+            if ($_GET['new_vote'] == "1") {
+                //Insert to vote table, prepare the query
+                $query = "INSERT INTO `rudbeck_info`.`eter0_wti_like_post` (`id`, `post_id`, `value`, `date_time`, `ip`, `user_id`) VALUES (NULL, '".$_GET['post_id']."', '1', '', '0', '0');";
+
+                $rs = mysqli_query($con,$query);
             }
-            
+
             $query2 = "SELECT * FROM `rudbeck_info`.`eter0_wti_like_post` where post_id=".$_GET['post_id']."";
             mysqli_query("SET names utf8;");
             mysqli_set_charset("utf8");
             mysqli_set_charset('utf8');
             $rs = mysqli_query($con,$query2);
-            
+
             $mysql_count_result = mysqli_num_rows($rs);
 
             $response["success"] = 1;
@@ -169,73 +199,73 @@ else if($_GET['post_vote'] == "1") {
             echo json_encode($response);
         }
     }
-else if ($_GET['list-all-courses'] == "1") {
-      $query = "SELECT * FROM `eter0_term_taxonomy` WHERE parent=".$_GET['parent']." ORDER BY `term_id` ASC";
-      mysqli_query("SET names utf8;");
-      mysqli_set_charset("utf8");
-      mysqli_set_charset('utf8');
-      $rs = mysqli_query($con,$query);
-      
-      $all_courses_posts_url= "http://eter.rudbeck.info/category/kurser/?json=1&count=10&apikey=ErtYnDsKATCzmuf6";
-      $json_elements = file_get_contents($all_courses_posts_url);
-      $elements_arr = json_decode($json_elements, TRUE);
-      //print_r($elements_arr);
-        
-    if (mysqli_num_rows($rs) > 0) {
-    $response["list_all_courses"]= array();
+    else if ($_GET['list-all-courses'] == "1") {
+        $query = "SELECT * FROM `eter0_term_taxonomy` WHERE parent=".$_GET['parent']." ORDER BY `term_id` ASC";
+        mysqli_query("SET names utf8;");
+        mysqli_set_charset("utf8");
+        mysqli_set_charset('utf8');
+        $rs = mysqli_query($con,$query);
 
-    while($obj = mysqli_fetch_array($rs)) {
-         $s_query = "SELECT * FROM `eter0_terms` WHERE term_id=".$obj['term_id']." ORDER BY `term_id` ASC";
-         mysqli_query("SET names utf8;");
-         mysqli_set_charset("utf8");
-         mysqli_set_charset('utf8');
-         $rs2 = mysqli_query($con,$s_query);
-        while($objs = mysqli_fetch_array($rs2)) {
-            $info = array(
-            "id" => $obj['term_id'], "description" => utf8_encode($obj['description']), "name" => utf8_encode($objs['name']), "parent" => utf8_encode($obj['parent']), "post-count" => utf8_encode($obj['count']), "slug" => utf8_encode($objs['slug'])
+        $all_courses_posts_url= "http://eter.rudbeck.info/category/kurser/?json=1&count=10&apikey=ErtYnDsKATCzmuf6";
+        $json_elements = file_get_contents($all_courses_posts_url);
+        $elements_arr = json_decode($json_elements, TRUE);
+        //print_r($elements_arr);
 
-        );
-        array_push($response["list_all_courses"], $info);  
-        }    
-    }
-    $response["success"] = 1;
-    header("Content-type: application/json; charset=utf-8");
-    header("Access-Control-Allow-Origin: *");
-    //print_r($response);
+        if (mysqli_num_rows($rs) > 0) {
+            $response["list_all_courses"]= array();
 
-    //Loop through all courses     
-    foreach($response['list_all_courses'] as $index => $course) {
-        //Loop through all posts in the wordpress api for courses category
-        foreach($elements_arr[posts] as $p => $arr) {
-            //Loop through the posts categories
-            foreach($arr[categories] as $arr2 => $cat) {
-                // If the category id matches the course id push the posts in to elments array ´
-                if($course['id'] == $cat['id']) {               
-                    array_push($response['list_all_courses'][$index]['elements'][] = $arr);
+            while($obj = mysqli_fetch_array($rs)) {
+                $s_query = "SELECT * FROM `eter0_terms` WHERE term_id=".$obj['term_id']." ORDER BY `term_id` ASC";
+                mysqli_query("SET names utf8;");
+                mysqli_set_charset("utf8");
+                mysqli_set_charset('utf8');
+                $rs2 = mysqli_query($con,$s_query);
+                while($objs = mysqli_fetch_array($rs2)) {
+                    $info = array(
+                        "id" => $obj['term_id'], "description" => utf8_encode($obj['description']), "name" => utf8_encode($objs['name']), "parent" => utf8_encode($obj['parent']), "post-count" => utf8_encode($obj['count']), "slug" => utf8_encode($objs['slug'])
+
+                    );
+                    array_push($response["list_all_courses"], $info);  
+                }    
+            }
+            $response["success"] = 1;
+            header("Content-type: application/json; charset=utf-8");
+            header("Access-Control-Allow-Origin: *");
+            //print_r($response);
+
+            //Loop through all courses     
+            foreach($response['list_all_courses'] as $index => $course) {
+                //Loop through all posts in the wordpress api for courses category
+                foreach($elements_arr[posts] as $p => $arr) {
+                    //Loop through the posts categories
+                    foreach($arr[categories] as $arr2 => $cat) {
+                        // If the category id matches the course id push the posts in to elments array ´
+                        if($course['id'] == $cat['id']) {               
+                            array_push($response['list_all_courses'][$index]['elements'][] = $arr);
+                        }
+                    }
                 }
             }
+            //encode the respone in json format
+            echo json_encode($response);
         }
-    }
-    //encode the respone in json format
-    echo json_encode($response);
-    }
-    else {
-        $response["success"] = 0;
-        $response["message"] = "Inga kurser hittades";
-        header("Content-type: application/json; charset=utf-8");
-        header("Access-Control-Allow-Origin: *");
-        echo json_encode($response);
-    }   
+        else {
+            $response["success"] = 0;
+            $response["message"] = "Inga kurser hittades";
+            header("Content-type: application/json; charset=utf-8");
+            header("Access-Control-Allow-Origin: *");
+            echo json_encode($response);
+        }   
     } else if($_GET['list-courses-name'] == "1"){
         $query = "SELECT * FROM `eter0_term_taxonomy` WHERE parent=".$_GET['parent']." ORDER BY `term_id` ASC";
         mysqli_query("SET names utf8;");
         mysqli_set_charset("utf8");
         mysqli_set_charset('utf8');
         $rs = mysqli_query($con,$query);
-        
+
         if (mysqli_num_rows($rs) > 0) {
             $response["list_courses"]= array();
-            
+
             while($obj = mysqli_fetch_array($rs)) {
                 $s_query = "SELECT * FROM `eter0_terms` WHERE term_id=".$obj['term_id']." ORDER BY `term_id` ASC";
                 mysqli_query("SET names utf8;");
@@ -244,8 +274,8 @@ else if ($_GET['list-all-courses'] == "1") {
                 $rs2 = mysqli_query($con,$s_query);
                 while($objs = mysqli_fetch_array($rs2)) {
                     $info = array("id" => $obj['term_id'], "description" => utf8_encode($obj['description']), "name" => utf8_encode($objs['name']), "parent" => utf8_encode($obj['parent']), "post-count" => utf8_encode($obj['count']), "slug" => utf8_encode($objs['slug'])
-                         );
-                array_push($response["list_courses"], $info);  
+                                 );
+                    array_push($response["list_courses"], $info);  
                 }    
             }
             $response["success"] = 1;
@@ -254,26 +284,26 @@ else if ($_GET['list-all-courses'] == "1") {
             echo json_encode($response);   
         }
     } else if($_GET['startpage']='1') {
-          $query = "SELECT * FROM `eter0_eter_start` ORDER BY id ASC";
-          mysqli_query("SET names utf8;");
-          mysqli_set_charset("utf8");
-          mysqli_set_charset('utf8');
-          $rs = mysqli_query($con,$query);
+        $query = "SELECT * FROM `eter0_eter_start` ORDER BY id ASC";
+        mysqli_query("SET names utf8;");
+        mysqli_set_charset("utf8");
+        mysqli_set_charset('utf8');
+        $rs = mysqli_query($con,$query);
 
         if (mysqli_num_rows($rs) > 0) {
-        $response["startpage"]= array();
+            $response["startpage"]= array();
 
-        while($obj = mysqli_fetch_array($rs)) {
+            while($obj = mysqli_fetch_array($rs)) {
                 $info = array(
-                "id" => $obj['id'], "title" => utf8_encode($obj['title']), "content" => utf8_encode($obj['content']), "image_url" => utf8_encode($obj['image_url']), "on_link" => utf8_encode($obj['on_link']),"row" => $obj['row'], "position" => $obj['position'], "is_dyn" => $obj['is_dyn'], "dyn_link" => utf8_encode($obj['dyn_link'])
+                    "id" => $obj['id'], "title" => utf8_encode($obj['title']), "content" => utf8_encode($obj['content']), "image_url" => utf8_encode($obj['image_url']), "on_link" => utf8_encode($obj['on_link']),"row" => $obj['row'], "position" => $obj['position'], "is_dyn" => $obj['is_dyn'], "dyn_link" => utf8_encode($obj['dyn_link'])
 
-        );
-         array_push($response["startpage"], $info);
-        }
+                );
+                array_push($response["startpage"], $info);
+            }
 
             $response["success"] = 1;
             header("Content-type: application/json; charset=utf-8");
-             header("Access-Control-Allow-Origin: *");
+            header("Access-Control-Allow-Origin: *");
             echo json_encode($response);
         }
         else {
@@ -284,13 +314,13 @@ else if ($_GET['list-all-courses'] == "1") {
             header("Access-Control-Allow-Origin: *");
             echo json_encode($response);
         }
-        }
     }
-    else {
-        $response["success"] = 0;
-        $response["message"] = "Felaktiga paramterar";
-        header("Content-type: application/json; charset=utf-8");
-        header("Access-Control-Allow-Origin: *");
-        echo json_encode($response);
-    }
+}
+else {
+    $response["success"] = 0;
+    $response["message"] = "Felaktiga paramterar";
+    header("Content-type: application/json; charset=utf-8");
+    header("Access-Control-Allow-Origin: *");
+    echo json_encode($response);
+}
 ?>
